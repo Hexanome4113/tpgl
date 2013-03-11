da83d01d9ee31446bff0a3c8ddd26d18518e6759
yann <yann@Wynock.(none)>
2013-03-11 11:27:29 +0100

    debut xmlnode.cpp

--- modele/xmlnode.cpp
+++ modele/xmlnode.cpp
@@ -2,7 +2,7 @@
 
 XMLNode::XMLNode()
 {
-
+	
 }
 
 XMLNode::~XMLNode()
@@ -10,3 +10,77 @@ XMLNode::~XMLNode()
 
 }
 
+string XMLNode::getNameSpace()
+{
+	return elementNamespace;
+}
+
+string XMLNode::getNodeName()
+{
+	return elementName;
+}
+
+string XMLNode::getFullName()
+{
+	return (elementNamespace+":"+elementName);
+}
+
+bool XMLNode::hasAttribute(string name)
+{
+	return attributes.find(name) != attributes.end();
+}
+
+
+bool XMLNode::hasMixedContent()
+{
+	if (children.empty())
+	{
+		return false;
+	}
+	else
+	{
+		bool compareValue = (*children.begin()).isTextNode();
+		for (map<string, string>::iterator it = attributes.begin() ; it != attributes.end() ; it++)
+		{
+			if (*it.isTextNode() != compareValue)
+				return true;
+		}
+		return false;
+	}
+}
+
+
+vector<XMLNode*> XMLNode::getChildren()
+{
+    return children;
+}
+
+XMLNode* XMLNode::getParent()
+{
+    return parent;
+}
+
+string XMLNode::getTextContent()
+{
+    return textContent;
+}
+
+XMLNode* XMLNode::getDocumentRoot()
+{
+    return documentRoot;
+}
+
+const map<string, string>& XMLNode::getAttributes()
+{
+    return attributes;
+}
+
+bool XMLNode::isTextNode()
+{
+    return ( nodeType == TEXT_NODE );
+}
+
+bool XMLNode::isElementNode()
+{
+    return ( nodeType == ELEMENT_NODE );
+}
