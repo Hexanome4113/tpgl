af4b080ae8e931e0f168754f36fed7a32464e1e7
Tournier Thibaut <ttournier1@if219-18.insa-lyon.fr>
2013-03-18 11:39:32 +0100

    Template : Ajout makefile + début algo

---
+++ transformation_d-arbre/main.cpp
@@ -0,0 +1,19 @@
+/* 3.5.2 Traitement des templates
+/*
+/**/
+
+/* --- Includes --- */
+#include <iostream>
+
+
+
+
+int main(){
+   /* Algorithme :
+   /*
+   /* - On part de la racine de l'arbre XML
+   /* - 
+   /*
+   /**/
+
+}
