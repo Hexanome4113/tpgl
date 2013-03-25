851c1b4e9b9b0733cd6769217ea263d428218a97
yann <yann@krayt.(none)>
2013-03-25 23:16:21 +0100

    doxygen applyXSLT

--- Code/src/applyxslt.h
+++ Code/src/applyxslt.h
@@ -1,8 +1,17 @@
 #ifndef APPLYXSLT_H
 #define APPLYXSLT_H
 
+
+/**
+ * \brief applique la feuille de style xslStylesheetRoot au document xmlDocumentRoot. La mémoire de l'arbre renvoyé est indépendante des arbres passés en paramètres.
+ * \param xslStylesheetRoot feuille de style XSLT à appliquer.
+ * \param xmlDocumentRoot document XML à transformer.
+ * \return Document après transformation. 
+ */
+XMLNode* applyXSLT(XMLNode *xmlDocumentRoot, XMLNode *xslStylesheetRoot);
+
+
 XMLNode* matchTemplates(XMLNode *xmlNode, XMLNode *xslRoot);
 vector<XMLNode*> applyTemplate(XMLNode *xmlNode, XMLNode *xslTemplate, XMLNode *xslRoot);
-XMLNode* applyXSLT(XMLNode *xmlDocumentRoot, XMLNode *xslStylesheetRoot);
 
 #endif // APPLYXSLT_H
